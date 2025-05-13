7716c95324e5988acd7cb00b762dd9f059f34eda
SJ <sj1004class@hanmail.net>
2025-05-13 22:58:50 +0900

    11주차 수업

---
+++ homepage/src/main/java/egovframework/let/template/web/TemplateController.java
@@ -0,0 +1,32 @@
+package egovframework.let.template.web;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import egovframework.com.cmm.LoginVO;
+import egovframework.com.cmm.util.EgovUserDetailsHelper;
+
+
+@Controller
+public class TemplateController {
+	
+	//헤더
+	@RequestMapping(value = "/template/header.do")
+	public String header(HttpServletRequest request, ModelMap model) throws Exception{
+		
+		LoginVO user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
+		model.addAttribute("USER_INFO", user);
+		
+		return "/template/Header";
+	}
+	
+	//푸터
+	@RequestMapping(value = "/template/footer.do")
+	public String footer(HttpServletRequest request, ModelMap model) throws Exception{
+		
+		return "/template/Footer";
+	}
+}
